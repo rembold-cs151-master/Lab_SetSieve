6adeec48b85c21dadbb56b6b572ec62d736fc934
Jed Rembold <jedediah.r@gmail.com>
2020-11-13 11:23:29 -0800

    Add initial resources

---
+++ Lab.py
@@ -0,0 +1,23 @@
+# Filename: Lab.py
+
+"""
+Implementation of the Sieve of Eratosthenes using sets.
+"""
+
+def sieve(nmax):
+    """
+    Implementation of the Sieve of Eratosthenes using sets.
+
+    Args:
+        nmax (int): upper limit of primes to be returned
+    Returns:
+        set: set of all prime numbers below nmax
+    """
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    print(sieve(20))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Lab_SetSieve
-Lab materials for Erastothenes Sieve with Sets
+# Lab 19: Sieving with Sets
+
+The goal of today's lab is to setup a simple Sieve of Eratosthenes utilizing what we have learned about sets. Full instructions on the posted webpage.
